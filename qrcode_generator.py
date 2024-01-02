b4ce23a57096720612201d3f241339c9ee859807
Farida Shakikhanli <turtleowl2005@users.noreply.github.com>
2024-01-02 11:49:36 +0400

    qrcode_generator.py

---
+++ qrcode_generator.py
@@ -0,0 +1,22 @@
+import qrcode
+
+class MyQr:
+    def __init__(self, size:int, padding:int):
+        self.qr = qrcode.QRCode(box_size=size, border=padding)
+
+    def create_qr(self, filename:str, fc:str, bc:str):
+        user_input = input("Enter text: ")
+        try:
+            self.qr.add_data(user_input)
+            qr_image = self.qr.make_image(fill_color=fc, back_color=bc)
+            qr_image.save(filename)
+            print(f"Successfully created ({filename})")
+        except Exception as e:
+            print(f"Error: {e}")
+
+def main():
+    myqr = MyQr(size=30, padding=4)
+    myqr.create_qr("sample.png", "pink", "white")
+
+if __name__ == '__main__':
+    main()
